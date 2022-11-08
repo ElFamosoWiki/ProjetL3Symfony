c3e92ae4b16a4d75f5bb60ad8b2a2bb4f2ab3ee6
ElFamosoWiki <83096409+ElFamosoWiki@users.noreply.github.com>
2022-11-08 13:22:03 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -221,6 +221,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setUrlImage(?ImageUser $category) : self
     {
         $this->UrlImage = $category;
+    }    
 
     /**
      * @return Collection<int, Event>
